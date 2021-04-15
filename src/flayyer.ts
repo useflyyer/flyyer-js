778806bc78ddab10930f6cf21b85e150f8c96040
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-04-15 17:58:49 -0400

    feat: Much better JSDocs

--- src/flayyer.ts
+++ src/flayyer.ts
@@ -1,77 +1,211 @@
 import { stringify, IStringifyOptions } from "qs";
 
-export type FlayyerMetaVariables = {
+/**
+ * Meta variables usually have values assigned by Flayyer depending on how and where images are rendered.
+ *
+ * You can force these values here.
+ * @example
+ * const meta: FlayyerMetaVariables ={
+ *   width: 1080, // in pixels
+ *   height: 1080, // in pixels
+ *   v: null, // disable cache-burst
+ *   id: "my-id", // analytics id
+ * }
+ * const flayyerIO = new FlayyerIO({ meta });
+ * const flayyerAI = new FlayyerAI({ meta });
+ * @example
+ * `https://flayyer.io/v2/flayyer/default/main.jpeg?__v=disabled`
+ * `https://flayyer.io/v2/flayyer/default/main.jpeg?_w=1080&_h=1080`
+ * `https://flayyer.ai/v2/flayyer-com/_/_/?__v=disabled`
+ * `https://flayyer.ai/v2/flayyer-com/_/_w=1080&_h=1080/marketplace`
+ */
+export interface FlayyerMetaVariables {
   /**
-   * Force crawler user agent
+   * Force crawler user agent.
+   * Converted to `_ua=` on `flayyer.href()` if set.
+   *
+   * Full list at https://docs.flayyer.com/docs/features/agent-detection
+   * @example
+   * "whatsapp" // _ua=whatsapp
+   * "facebook" // _ua=facebook
+   * "twitter" // _ua=twitter
+   * "instagram" // _ua=instagram
    */
   agent?: string | null;
+  /**
+   * Pixels (integer value).
+   * Converted to `_w=` on `flayyer.href()` if set.
+   * @example
+   * 1200 // _w=1200 // default value most of the time
+   * 1080 // _w=1080
+   * @example
+   * `https://flayyer.io/v2/flayyer/default/main.jpeg?_w=1080&_h=1080`
+   * `https://flayyer.ai/v2/flayyer-com/_/_w=1080&_h=1080/jobs`
+   */
   width?: string | number | null;
+  /**
+   * Pixels (integer value).
+   * Converted to `_h=` on `flayyer.href()` if set.
+   * @example
+   * 1200 // _h=630 // default value most of the time
+   * 1080 // _h=1080
+   * 1080 // _h=1920
+   * @example
+   * `https://flayyer.io/v2/flayyer/default/main.jpeg?_w=1080&_h=1080`
+   * `https://flayyer.ai/v2/flayyer-com/_/_w=1080&_h=1080/jobs`
+   */
   height?: string | number | null;
   /**
    * Range from [0.0, 1.0]
+   * Converted to `_res=` on `flayyer.href()` if set.
    */
   resolution?: string | number | null;
   /**
    * To identify your links on the analytics report
+   * Converted to `__id=` on `flayyer.href()` if set.
    */
   id?: string | number | null;
   /**
-   * Cache invalidator
+   * Cache invalidator, set to `null` or empty string `""` to disable it.
+   * Converted to `__v=` on `flayyer.href()` if set.
+   *
+   * **If you are using Flayyer inside your website for to render images we recommend disabling it to use browser's cache.**
+   * @example
+   * const flayyer = new FlayyerIO({ meta: { v: null } }); // disabled
+   * @example
+   * __v=null // disabled
+   * __v="1" // constant
+   * __v=undefined // `__v=123123` a timestamp will be used.
+   * @example
+   * `https://flayyer.io/v2/flayyer/default/main.jpeg?title=Hello&__v=123123` // by default is a timestamp
+   * `https://flayyer.io/v2/flayyer/default/main.jpeg?title=Hello&__v=` // disabled to use browser's cache
    */
   v?: string | number | null;
-};
+}
 
+/**
+ * Any possible object. **Remember: keys and values must be serializable.**
+ */
 export type FlayyerVariables =
   | {
       [key: string]: any;
     }
   | Record<any, any>;
 
+/**
+ * Supported extensions
+ * @example
+ */
 export type FlayyerExtension = "jpeg" | "jpg" | "png" | "webp";
 
-export type FlayyerIOParams<T extends FlayyerVariables> = {
+export interface FlayyerCommonParams<T extends FlayyerVariables> {
   /**
-   * Visit https://app.flayyer.com to get this value for your project
-   */
-  tenant: string;
-
-  /**
-   * Visit https://app.flayyer.com to get this value for your project
-   */
-  deck: string;
-
-  /**
-   * Visit https://app.flayyer.com to get this value for your project
-   */
-  template: string;
-
-  /**
-   * Optional. Leave empty to always grab the latest version.
+   * Optional. Leave empty `""` or as `_` to always grab the latest version.
    */
   version?: number | null;
 
+  /**
+   * Supported extensions are: `"jpeg" | "jpg" | "png" | "webp"`
+   */
   extension?: FlayyerExtension | null;
 
   /**
    * JS serializable variables.
+   * @example
+   * const flayyerIO = new FlayyerIO({ variables: { title: "Hello world", image: "https://example.com/logo.png" } })
+   * console.log(flayyerIO.href())
+   * // https://flayyer.io/v2/TENANT/DECK/TEMPLATE.jpeg?title=Hello+world&image=https%3A%2F%2Fexample.com%2Flogo.png
+   * @example
+   * const flayyerAI = new FlayyerAI({ variables: { title: "Hello world", image: "https://example.com/logo.png" } })
+   * console.log(flayyerAI.href())
+   * // https://flayyer.ai/v2/flayyer-com/_/title=Hello+world&image=https%3A%2F%2Fexample.com%2Flogo.png/
    */
   variables?: T | null;
 
   /**
-   * Extra parameters that adds data for analytics or changes meta-information of the rendered images such as width and height.
+   * Meta variables usually have values assigned by Flayyer depending on how and where images are rendered.
+   *
+   * You can force these values here.
+   * @example
+   * const meta: FlayyerMetaVariables ={
+   *   width: 1080, // in pixels
+   *   height: 1080, // in pixels
+   *   v: null, // disable cache-burst
+   *   id: "my-id", // analytics id
+   * }
+   * const flayyerIO = new FlayyerIO({ meta });
+   * const flayyerAI = new FlayyerAI({ meta });
+   * @example
+   * `https://flayyer.io/v2/flayyer/default/main.jpeg?__v=disabled`
+   * `https://flayyer.io/v2/flayyer/default/main.jpeg?_w=1080&_h=1080`
+   * `https://flayyer.ai/v2/flayyer-com/_/_/?__v=disabled`
+   * `https://flayyer.ai/v2/flayyer-com/_/_w=1080&_h=1080/marketplace`
    */
   meta?: FlayyerMetaVariables | null;
-};
+}
 
-export class FlayyerIO<T extends FlayyerVariables = FlayyerVariables> {
+/**
+ * These are the parameters required (some are optional) to create an URL that will render Flayyer images.
+ *
+ * Required is: `tenant`, `deck`, `template`.
+ *
+ * Set and override the variables of the template by using the `variables` object.
+ *
+ * Example: https://flayyer.io/v2/flayyer/default/main.jpeg?title=Thanks+for+reading+this
+ * @example
+ * const flayyer = new FlayyerIO({
+ *   tenant: "flayyer",
+ *   deck: "default",
+ *   template: "main",
+ *   variables: { title: "Thanks for reading this" },
+ *  });
+ *  console.log(flayyer.href())
+ * // https://flayyer.io/v2/flayyer/default/main.jpeg?title=Thanks+for+reading+this
+ */
+export interface FlayyerIOParams<T extends FlayyerVariables> extends FlayyerCommonParams<T> {
+  /**
+   * Your tenant's `slug`. Lowercase and no spaces.
+   * Visit https://flayyer.com/dashboard to get this value for your project
+   */
+  tenant: string;
+
+  /**
+   * Your deck's `slug`. Lowercase and no spaces.
+   * Visit https://flayyer.com/dashboard to get this value for your project
+   */
+  deck: string;
+
+  /**
+   * Your template's `slug`. Lowercase and no spaces.
+   * Visit https://flayyer.com/dashboard to get this value for your project
+   */
+  template: string;
+}
+
+/**
+ * This class helps you creating URLs that will render Flayyer images.
+ *
+ * Required is: `tenant`, `deck`, `template`.
+ *
+ * Set and override the variables of the template by using the `variables` object.
+ *
+ * Example: https://flayyer.io/v2/flayyer/default/main.jpeg?title=Thanks+for+reading+this
+ * @example
+ * const flayyer = new FlayyerIO({
+ *   tenant: "flayyer",
+ *   deck: "default",
+ *   template: "main",
+ *   variables: { title: "Thanks for reading this" },
+ *  });
+ *  console.log(flayyer.href())
+ * // https://flayyer.io/v2/flayyer/default/main.jpeg?title=Thanks+for+reading+this
+ */
+export class FlayyerIO<T extends FlayyerVariables = FlayyerVariables> implements FlayyerIOParams<T> {
   public tenant: string;
   public deck: string;
   public template: string;
   public version: number | null;
   public extension: FlayyerExtension;
-  /**
-   * JS serializable variables.
-   */
   public variables: T;
   public meta: FlayyerMetaVariables;
 
@@ -87,6 +221,9 @@ export class FlayyerIO<T extends FlayyerVariables = FlayyerVariables> {
     this.meta = args.meta || {};
   }
 
+  /**
+   * Returns a new instance. Values are shallow cloned.
+   */
   clone<K extends FlayyerVariables = T>(args?: Partial<FlayyerIOParams<K>>): FlayyerIO<K> {
     const next = new FlayyerIO<K>(Object.assign({}, this, args));
     return next;
@@ -106,7 +243,11 @@ export class FlayyerIO<T extends FlayyerVariables = FlayyerVariables> {
 
   /**
    * Generate final URL you can use in your og:images.
-   * @example <meta property="og:image" content={flayyer.href()} />
+   * @example
+   * <meta property="og:image" content={flayyer.href()} />
+   * @example
+   * const flayyer = new FlayyerIO({ meta: { v: null } });
+   * <img src={flayyer.href()} />
    */
   href(): string {
     if (isUndefined(this.tenant)) throw new Error("Missing 'tenant' property");
@@ -122,52 +263,58 @@ export class FlayyerIO<T extends FlayyerVariables = FlayyerVariables> {
   }
 
   /**
-   * Generate final URL you can use in your og:images.
-   * @example <meta property="og:image" content={flayyer.href()} />
+   * Alias of `.href()`
    */
   toString() {
     return this.href();
   }
 }
 
-export type FlayyerAIParams<T extends FlayyerVariables> = {
+/**
+ * This class helps you creating URLs that will render Flayyer images.
+ *
+ * Required is: `project`.
+ *
+ * Set default variables by using the `variables` object.
+ *
+ * Example: https://flayyer.ai/v2/flayyer-com/_/_/marketplace/simple-fade
+ * @example
+ * const flayyer = new FlayyerAI({
+ *   project: "flayyer-com",
+ *   route: "simple-fade",
+ *  });
+ *  console.log(flayyer.href())
+ * // https://flayyer.ai/v2/flayyer-com/_/_/marketplace/templates/simple-fade
+ */
+export interface FlayyerAIParams<T extends FlayyerVariables> extends FlayyerCommonParams<T> {
   /**
-   * Visit https://app.flayyer.com to get this value for your project
+   * Your project's `slug`. Lowercase and no spaces.
+   * Visit https://flayyer.com/dashboard to get this value for your project
    */
   project: string;
 
   /**
    * Current page path we will use in conjunction with the base URL defined in the Flayyer's Dashboard of your project.
+   * @example
+   * const flayyer = new FlayyerAI({ path: "/" });
+   * @example
+   * const flayyer = new FlayyerAI({ path: "/about" });
+   * @example
+   * const flayyer = new FlayyerAI({ path: "/products/1" });
+   * @example
+   * const flayyer = new FlayyerAI({ path: ["products", "1"] });
+   * @example
+   * const flayyer = new FlayyerAI({ path: "/page?id=1" });
    */
   path?: FlayyerPath;
-
-  /**
-   * Optional. Leave empty to always grab the latest version.
-   */
-  version?: number | null;
-
-  extension?: FlayyerExtension | null;
-
-  /**
-   * JS serializable variables.
-   */
-  variables?: T | null;
-
-  /**
-   * Extra parameters that adds data for analytics or changes meta-information of the rendered images such as width and height.
-   */
-  meta?: FlayyerMetaVariables | null;
-};
+}
 
 export type FlayyerPath = string | number | null | undefined | (string | number | null | undefined)[];
 
-export class FlayyerAI<T extends FlayyerVariables = FlayyerVariables> {
+export class FlayyerAI<T extends FlayyerVariables = FlayyerVariables> implements FlayyerAIParams<T> {
   public project: string;
   public path?: FlayyerPath;
   public extension: FlayyerExtension;
-  /**
-   * JS serializable variables.
-   */
   public variables: T;
   public meta: FlayyerMetaVariables;
 
@@ -181,6 +328,9 @@ export class FlayyerAI<T extends FlayyerVariables = FlayyerVariables> {
     this.meta = args.meta || {};
   }
 
+  /**
+   * Returns a new instance. Values are shallow cloned.
+   */
   clone<K extends FlayyerVariables = T>(args?: Partial<FlayyerAIParams<K>>): FlayyerAI<K> {
     const next = new FlayyerAI<K>(Object.assign({}, this, args));
     return next;
@@ -200,7 +350,11 @@ export class FlayyerAI<T extends FlayyerVariables = FlayyerVariables> {
 
   /**
    * Generate final URL you can use in your og:images.
-   * @example <meta property="og:image" content={flayyer.href()} />
+   * @example
+   * <meta property="og:image" content={flayyer.href()} />
+   * @example
+   * const flayyer = new FlayyerAI({ meta: { v: null } });
+   * <img src={flayyer.href()} />
    */
   href(): string {
     if (isUndefined(this.project)) throw new Error("Missing 'project' property");
@@ -213,8 +367,7 @@ export class FlayyerAI<T extends FlayyerVariables = FlayyerVariables> {
   }
 
   /**
-   * Generate final URL you can use in your og:images.
-   * @example <meta property="og:image" content={flayyer.href()} />
+   * Alias of `.href()`
    */
   toString() {
     return this.href();
@@ -231,6 +384,9 @@ export const Flayyer = FlayyerIO;
  */
 export default FlayyerIO;
 
+/**
+ * Internally used to convert an object to querystring.
+ */
 export function toQuery(variables: any, options: IStringifyOptions = { addQueryPrefix: false, format: "RFC1738" }) {
   return stringify(variables, options);
 }
