57c5b3445d2e98c9a2e0e8783d7973e7c76b418b
Franco Méndez <f@francomendez.com>
2021-10-20 15:35:39 -0300

    chore: Expose decode JWT method for testing

--- packages/flyyer/src/index.ts
+++ packages/flyyer/src/index.ts
@@ -8,7 +8,7 @@ export { normalizePath, FlyyerPath } from "@flyyer/flyyer-lite";
 export { FlyyerParams } from "@flyyer/flyyer-lite";
 export { FlyyerRenderParams } from "@flyyer/flyyer-lite";
 
-export { CREATE_JWT_TOKEN, SIGN_JWT_TOKEN, SIGN_HMAC_DATA, BASE64_URL } from "./jwt";
+export { CREATE_JWT_TOKEN, SIGN_JWT_TOKEN, SIGN_HMAC_DATA, BASE64_URL, DECODE_JWT_TOKEN } from "./jwt";
 export { Flyyer } from "./flyyer-signed";
 export { FlyyerRender } from "./flyyer-render-signed";
 export { isEqualFlyyer, isEqualFlyyerMeta, isEqualFlyyerRender } from "./compare";
